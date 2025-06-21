5ecab868c630c8994664341e9a5b756cb3048b84
CocoTeoh <teoh2004@gmail.com>
2025-06-21 17:07:19 +0800

    Bloomin logo resize

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -18,7 +18,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xFF735276),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -22,17 +22,20 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xFF735276),
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Image.asset(
-              "assets/img/Bloomin_logo.png",
-              width: double.infinity,
-              height: 300,
-              fit: BoxFit.cover,
-            ),
+          children: [Padding(
+              padding: const EdgeInsets.only(top: 70, bottom: 10),
+              child:Center(
+                child: Image.asset(
+                  "assets/img/Bloomin_logo.png",
+                  width: 350,
+                  fit: BoxFit.contain,
+                ),
+              )
+          ),
             const SizedBox(height: 18),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 50),
@@ -42,7 +45,7 @@ class _SignupPageState extends State<SignupPage> {
                   const Text(
                     'Sign up',
                     style: TextStyle(
-                      color: Color(0xFF755DC1),
+                      color: Color(0xFF29264C),
                       fontSize: 27,
                       fontFamily: 'Poppins',
                       fontWeight: FontWeight.w500,
@@ -56,8 +59,10 @@ class _SignupPageState extends State<SignupPage> {
                     textAlign: TextAlign.center,
                     decoration: const InputDecoration(
                       labelText: 'Username',
+                      filled: true,
+                      fillColor: Color(0xFFE7DAF5),
                       labelStyle: TextStyle(
-                        color: Color(0xFF755DC1),
+                        color: Color(0xFF786B89),
                         fontSize: 15,
                         fontFamily: 'Poppins',
                         fontWeight: FontWeight.w600,
@@ -80,8 +85,10 @@ class _SignupPageState extends State<SignupPage> {
                     textAlign: TextAlign.center,
                     decoration: const InputDecoration(
                       labelText: 'Email',
+                      filled: true,
+                      fillColor: Color(0xFFE7DAF5),
                       labelStyle: TextStyle(
-                        color: Color(0xFF755DC1),
+                        color: Color(0xFF786B89),
                         fontSize: 15,
                         fontFamily: 'Poppins',
                         fontWeight: FontWeight.w600,
@@ -115,8 +122,10 @@ class _SignupPageState extends State<SignupPage> {
                           });
                         },
                       ),
-                      labelStyle: const TextStyle(
-                        color: Color(0xFF755DC1),
+                      filled: true,
+                      fillColor: Color(0xFFE7DAF5),
+                      labelStyle: TextStyle(
+                        color: Color(0xFF786B89),
                         fontSize: 15,
                         fontFamily: 'Poppins',
                         fontWeight: FontWeight.w600,
@@ -150,8 +159,10 @@ class _SignupPageState extends State<SignupPage> {
                           });
                         },
                       ),
-                      labelStyle: const TextStyle(
-                        color: Color(0xFF755DC1),
+                      filled: true,
+                      fillColor: Color(0xFFE7DAF5),
+                      labelStyle: TextStyle(
+                        color: Color(0xFF786B89),
                         fontSize: 15,
                         fontFamily: 'Poppins',
                         fontWeight: FontWeight.w600,
@@ -188,7 +199,7 @@ class _SignupPageState extends State<SignupPage> {
                         child: const Text(
                           'Create account',
                           style: TextStyle(
-                            color: Colors.white,
+                            color: Color(0xFFE5D0FF),
                             fontSize: 15,
                             fontFamily: 'Poppins',
                             fontWeight: FontWeight.w500,
@@ -205,7 +216,7 @@ class _SignupPageState extends State<SignupPage> {
                       const Text(
                         'Have an account?',
                         style: TextStyle(
-                          color: Color(0xFF837E93),
+                          color: Color(0xFFAB95CA),
                           fontSize: 15,
                           fontFamily: 'Poppins',
                           fontWeight: FontWeight.w500,
@@ -223,7 +234,7 @@ class _SignupPageState extends State<SignupPage> {
                         child: const Text(
                           'Log In',
                           style: TextStyle(
-                            color: Color(0xFF755DC1),
+                            color: Color(0xFF846BD6),
                             fontSize: 15,
                             fontFamily: 'Poppins',
                             fontWeight: FontWeight.w500,
